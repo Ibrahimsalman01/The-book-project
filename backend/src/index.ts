14624ba1079f74a731f62b4c146b09dca94be8fe
Bao Le <letuanbao2020@gmail.com>
2024-06-06 17:29:07 -0400

    06/06/2024 - Created User Repo and Builder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,29 +3,44 @@ import * as schema from "./schema/schema.js";
 import pkg from 'pg';
 import { BookRepository } from "./services/book.repository.js";
 import { BookBuilder } from "./services/book.builder.js";
+import { UserRepository } from "./services/user.repository.js";
+import { UserBuilder } from "./services/user.builder.js";
 import { DatabaseService } from "./utils/database.config.js";
 
 DatabaseService.drizzleInit();
 
 async function test() {
 	try {
-		// const newBook = new BookBuilder()
-		// .setTitle('SCARY2')
-		// .setAuthor('Vegeta')
-		// .setSynopsis('A synopsis')
-		// .setGenres(['Thriller', 'Depression']) // LIMIT NUMBER OF GENRES USER CAN PUT
-		// .setCoverImage('test')
-		// .setStory('This is the story of the new book.')
-		// .build();
+		/* const newBook = new BookBuilder()
+		.setTitle('SCARY2')
+		.setAuthor('Vegeta')
+		.setSynopsis('A synopsis')
+		.setGenres(['Thriller', 'Depression']) //LIMIT NUMBER OF GENRES USER CAN PUT
+		.setCoverImage('test')
+		.setStory('This is the story of the new book.')
+		.build(); */
+
+    const newUser = new UserBuilder()
+    .setUsername("baoinadaze")
+    .setEmail("bao@gmail.com")
+    .setPassword("1234")
+    .build();
+
+    const userRepo = new UserRepository();
+
+    //await userRepo.registerUser(newUser)
+    await userRepo.loginUser("bao@gmail.com", "1234")
 
 
-		const bookRepo = new BookRepository();
+		//const bookRepo = new BookRepository();
 		// const test1 = await bookRepo.getAllBooks();
 		// const test2 = await bookRepo.getBook(1);
 		// console.log(test1)
 
-    const deletedBookId = await bookRepo.deleteBook(104);
-    console.log(deletedBookId);
+
+
+    //const deletedBookId = await bookRepo.deleteBook(104);
+    //console.log(deletedBookId);
 		// await bookRepo.createBook(newBook); // createBook should return the book so we can add to the frontend
 		DatabaseService.closePool();
 	} catch (error) {
