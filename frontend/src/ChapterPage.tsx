54caebc3186357ab22201a69c1aada5eb5a0f8d4
Ibrahim Salman <ibrahimsalman01@gmail.com>
2024-07-10 16:59:56 -0400

    house keeping done and added navigation

--- frontend/src/ChapterPage.tsx
+++ frontend/src/ChapterPage.tsx
@@ -1,17 +1,24 @@
 import { useCallback, useEffect, useState } from "react";
 import { getChapter, getTotalChapters } from "../services/novels";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import "./App.css";
+import { ChapterNavigationBar } from "./components/ChapterNagivationBar";
+import "./ChapterPage.css";
 
-const App = () => {
-  const [chapter, setChapter] = useState<string[]>([]);
+export interface novelObject {
+  novelId: number,
+  chapterId: number,
+  seriesName: string,
+  chapterNumber: number,
+  pages: string[]
+}
+
+const ChapterPage = () => {
+  const [chapter, setChapter] = useState<novelObject>();
   const [currentChapter, setCurrentChapter] = useState<number>(1);
   const [totalChapters, setTotalChapters] = useState<number>(0); //used to hold current number of chapters for novel
 
   const getNovelChapter = useCallback(async (novelId: number, chapterNumber: number) => {
     const response = await getChapter(novelId, chapterNumber);
-    setChapter(response.pages);
+    setChapter(response as novelObject);
   }, []);
 
   const handlePrevChapter = () => {
@@ -33,7 +40,7 @@ const App = () => {
 
   useEffect(() => {  
     fetchTotalChapters();
-    getNovelChapter(1, currentChapter); 
+    getNovelChapter(1, currentChapter);
     
   }, [getNovelChapter, currentChapter]);
 
@@ -42,12 +49,18 @@ const App = () => {
       <div className="top-bar">
         The Book Project
       </div>
-      <div className="chapter-buttons">
-        <button onClick={handlePrevChapter}>Prev</button>
-        <button onClick={handleNextChapter}>Next</button>
-        </div>
+      <div className="series-info">
+        {`Home > ${chapter?.seriesName} > Chapter ${currentChapter}`}
+      </div>
+      <ChapterNavigationBar
+        handlePrevChapter={handlePrevChapter}
+        handleNextChapter={handleNextChapter}
+        currentChapter={currentChapter}
+        totalChapters={totalChapters}
+        selectBarChapter={(e) => setCurrentChapter(Number(e.target.value.split(" ")[1]))}
+      />
       <div className="chapter">
-        {chapter.map((page, index) => (
+        {chapter?.pages.map((page, index) => (
           <img key={index} src={page} alt={`Page ${index + 1}`} />
         ))}
       </div>
@@ -55,4 +68,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default ChapterPage;
